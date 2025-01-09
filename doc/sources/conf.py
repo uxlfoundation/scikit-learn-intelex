8a6bbdfc9626375e1b7502fceb2980fe547c7fce
yue.jiao <yuejiao@smtp.intel.com>
2025-01-08 17:08:06 -0800

    fix: import daal4py and sklearn to path in conf.py for both daal4py and sklearn doc build

--- doc/sources/conf.py
+++ doc/sources/conf.py
@@ -33,7 +33,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../"))
-sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("../../daal4py"))
+sys.path.insert(0, os.path.abspath("../../sklearnex"))
 
 
 # -- Project information -----------------------------------------------------
