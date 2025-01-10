b910ada8832a30cb8695ae78db40d866d200c5f1
yue.jiao <yuejiao@smtp.intel.com>
2025-01-09 21:53:48 -0800

    fix: test need the lib folder

--- doc/daal4py/conf.py
+++ doc/daal4py/conf.py
@@ -33,7 +33,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath("../../build"))
+sys.path.insert(0, os.path.abspath("../../build/lib.linux-x86_64-cpython-311"))
 
 # -- Project information -----------------------------------------------------
 
