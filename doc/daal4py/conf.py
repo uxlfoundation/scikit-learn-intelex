17dc7c9d42536e9cd25771568ec4873de83a8a64
yue.jiao <yuejiao@smtp.intel.com>
2025-01-10 17:08:24 -0800

    test-ci: print the sys path library path

--- doc/daal4py/conf.py
+++ doc/daal4py/conf.py
@@ -33,7 +33,6 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath("../../build/lib.linux-x86_64-cpython-311"))
 
 # -- Project information -----------------------------------------------------
 
