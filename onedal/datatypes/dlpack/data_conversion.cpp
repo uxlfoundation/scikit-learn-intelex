826b2a4a0475df10e4757ebf5dcc02ae1fc654d4
Faust, Ian <ian.faust@intel.com>
2025-02-03 14:29:25 +0100

    address changes

--- onedal/datatypes/dlpack/data_conversion.cpp
+++ onedal/datatypes/dlpack/data_conversion.cpp
@@ -26,9 +26,7 @@
 namespace oneapi::dal::python::dlpack {
 
 template <typename T, typename managed_t>
-inline dal::homogen_table convert_to_homogen_impl(managed_t* dlm_tensor,
-                                                  bool readonly,
-                                                  py::object q_obj) {
+inline dal::homogen_table convert_to_homogen_impl(managed_t* dlm_tensor, py::object q_obj) {
     dal::homogen_table res{};
 
     DLTensor tensor = dlm_tensor->dl_tensor;
@@ -42,13 +40,12 @@ inline dal::homogen_table convert_to_homogen_impl(managed_t* dlm_tensor,
     // use static cast because data is known to be void*, and reintrepret_cast
     // since we know that T* is of a set specific types that we will guarantee
     // for the compiler.
-    const auto* const ptr =
-        reinterpret_cast<const T*>(static_cast<char*>(tensor.data) + tensor.byte_offset);
+    char* raw_ptr = static_cast<char*>(tensor.data);
+    const T* const ptr = reinterpret_cast<const T*>(raw_ptr + tensor.byte_offset);
 
     // get shape, if 1 dimensional, force col count to 1
-    std::int64_t row_count, col_count;
-    row_count = tensor.shape[0];
-    col_count = get_ndim(tensor) == 1 ? 1l : tensor.shape[1];
+    std::int64_t row_count = tensor.shape[0];
+    std::int64_t col_count = get_ndim(tensor) == 1 ? 1l : tensor.shape[1];
 
     // get data layout for homogeneous check
     const dal::data_layout layout = get_dlpack_layout(tensor);
@@ -66,52 +63,37 @@ inline dal::homogen_table convert_to_homogen_impl(managed_t* dlm_tensor,
     // unsupported device it will throw an error.
     if (check_dlpack_oneAPI_device(tensor.device.device_type)) {
 #ifdef ONEDAL_DATA_PARALLEL
-        // if located on a SYCL device, use the queue.
-#define MAKE_QUEUED_HOMOGEN(pointer)                     \
-    res = dal::homogen_table(queue,                      \
-                             pointer,                    \
-                             row_count,                  \
-                             col_count,                  \
-                             deleter,                    \
-                             std::vector<sycl::event>{}, \
-                             layout);
-        // generate queue from dlpack device information
+        // if located on a SYCL device, use the queue. Generate queue from dlpack device information.
         // If a queue is given, it will override the general queue that would be generated
-        // from get_queue_by_device_id. Note, this behavior only occurs if on a oneAPI device.
+        // from get_queue_by_device_id. This is important as dlpack oneAPI devices are not aware of
+        // sub-devices, priority queues or sycl contexts, which can cause the tensor only to be usable
+        // in oneDAL when sycl default contexts and parent devices are used. It is assumed that the
+        // external queue  is a more specific which properly handles these cases (allowing for oneDAL).
         sycl::queue queue;
         queue = !q_obj.is(py::none()) ? get_queue_from_python(q_obj)
                                       : get_queue_by_device_id(tensor.device.device_id);
 
-        if (readonly) {
-            MAKE_QUEUED_HOMOGEN(ptr);
-        }
-        else {
-            auto* const mut_ptr = const_cast<T*>(ptr);
-            MAKE_QUEUED_HOMOGEN(mut_ptr);
-        }
-        return res;
+        res = dal::homogen_table(queue,
+                                 ptr,
+                                 row_count,
+                                 col_count,
+                                 deleter,
+                                 std::vector<sycl::event>{},
+                                 layout);
 #else
         throw std::invalid_argument(
             "Input array located on a oneAPI device, but sklearnex installation does not have SYCL support.");
 #endif
     }
-
-    if (readonly) {
+    else {
         res = dal::homogen_table(ptr, row_count, col_count, deleter, layout);
     }
-    else {
-        auto* const mut_ptr = const_cast<T*>(ptr);
-        res = dal::homogen_table(mut_ptr, row_count, col_count, deleter, layout);
-    }
-
     return res;
 }
 
 dal::table convert_to_table(py::object obj, py::object q_obj) {
     dal::table res;
     bool versioned = false;
-    // Versioned has a readonly flag that can be used to block modification
-    bool readonly = false;
     DLManagedTensor* dlm;
     DLManagedTensorVersioned* dlmv;
     DLTensor tensor;
@@ -136,7 +118,6 @@ dal::table convert_to_table(py::object obj, py::object q_obj) {
         }
         tensor = dlmv->dl_tensor;
         versioned = true;
-        readonly = (dlmv->flags & DLPACK_FLAG_BITMASK_READ_ONLY) != 0;
     }
     else {
         throw std::runtime_error("unable to extract dltensor");
@@ -150,7 +131,7 @@ dal::table convert_to_table(py::object obj, py::object q_obj) {
     if (!q_obj.is(py::none()) && !q_obj.attr("sycl_device").attr("has_aspect_fp64").cast<bool>() &&
         dtype == dal::data_type::float64) {
         // If the queue exists, doesn't have the fp64 aspect, and the data is float64
-        // then cast it to float32 (using reduce_precision)
+        // then cast it to float32 (using reduce_precision), error raised in reduce_precision
         py::object copy = reduce_precision(obj);
         res = convert_to_table(copy, q_obj);
         return res;
@@ -167,10 +148,9 @@ dal::table convert_to_table(py::object obj, py::object q_obj) {
         return res;
     }
 
-#define MAKE_HOMOGEN_TABLE(CType)                                                               \
-    res = versioned                                                                             \
-              ? convert_to_homogen_impl<CType, DLManagedTensorVersioned>(dlmv, readonly, q_obj) \
-              : convert_to_homogen_impl<CType, DLManagedTensor>(dlm, readonly, q_obj);
+#define MAKE_HOMOGEN_TABLE(CType)                                                           \
+    res = versioned ? convert_to_homogen_impl<CType, DLManagedTensorVersioned>(dlmv, q_obj) \
+                    : convert_to_homogen_impl<CType, DLManagedTensor>(dlm, q_obj);
     SET_CTYPE_FROM_DAL_TYPE(dtype,
                             MAKE_HOMOGEN_TABLE,
                             throw std::invalid_argument("Found unsupported array type"));
