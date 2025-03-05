74cf3a7bf379b08a9919f16c92aa14910951ac4d
Andreas Huber <andreas.huber@intel.com>
2025-03-04 23:44:33 -0800

    formatting / remove default values

--- onedal/_config.py
+++ onedal/_config.py
@@ -23,20 +23,20 @@ Default values for global configuration parameters.
 These values are typically managed through the sklearnex.set_config() interface.
 Here we only define the defaults.
 
-target_offload : string or dpctl.SyclQueue, default=None
+target_offload:
     The device primarily used to perform computations.
     If string, expected to be "auto" (the execution context
     is deduced from input data location), or SYCL* filter selector string.
     Global default: "auto".
-allow_fallback_to_host : bool, default=None
+allow_fallback_to_host:
     If True, allows to fallback computation to host device
     in case particular estimator does not support the selected one.
     Global default: False.
-allow_sklearn_after_onedal : bool, default=None
+allow_sklearn_after_onedal:
     If True, allows to fallback computation to sklearn after onedal
     backend in case of runtime error on onedal backend computations.
     Global default: True.
-use_raw_input : bool, default=None
+use_raw_input:
     If True, uses the raw input data in the onedal backend computations
     without any checks on data consistency or validity.
     Note: This option is not recommended for general use.
