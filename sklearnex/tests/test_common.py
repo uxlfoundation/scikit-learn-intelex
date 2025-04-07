9704e19e8c6de3b047a6cd7f38407557ed67eb4e
Ian Faust <icfaust@gmail.com>
2025-04-07 20:25:55 +0200

    Update test_common.py

--- sklearnex/tests/test_common.py
+++ sklearnex/tests/test_common.py
@@ -193,11 +193,11 @@ def test_oneDALEstimator_inheritance(monkeypatch):
             ), f"{name} does not inherit the oneDALEstimator"
             # oneDAL estimator should be inherited from before BaseEstimator
             mro = obj.__mro__
-            assert mro.index(oneDALEstimator) < mro.index(BaseEstimator), "incorrect mro in {name}"
+            assert mro.index(oneDALEstimator) < mro.index(BaseEstimator), f"incorrect mro in {name}"
             if not any([issubclass(obj, est) for est in UNPATCHED_MODELS.values()]):
                 assert (
                     mro[mro.index(oneDALEstimator) + 1] is BaseEstimator
-                ), "oneDALEstimator should be inherited just before BaseEstimator in {name}"
+                ), f"oneDALEstimator should be inherited just before BaseEstimator in {name}"
 
 
 def _fullpath(path):
