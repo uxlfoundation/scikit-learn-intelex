293922d060033c25ec6ff987638d1d8800b33027
Ian Faust <icfaust@gmail.com>
2025-04-15 20:36:06 +0200

    Update base.py

--- sklearnex/base.py
+++ sklearnex/base.py
@@ -80,13 +80,13 @@ class oneDALEstimator:
             # the equivalent sklearn estimator, if it is BaseEstimator, it is a
             # sklearnex-only estimator.
             url = BaseEstimator._get_doc_link(self)
-            if ( 
+            if (
                 url
                 and oneDALEstimator in mro
                 and mro[mro.index(oneDALEstimator) + 1] is BaseEstimator
             ):
                 module_path, _ = self.__class__.__module__.rsplit(".", 1)
                 class_name = self.__class__.__name__
-                url = f"https://uxlfoundation.github.io/scikit-learn-intelex/latest/non-scikit-algorithms.html#{module_path}.{class_name}"                
+                url = f"https://uxlfoundation.github.io/scikit-learn-intelex/latest/non-scikit-algorithms.html#{module_path}.{class_name}"
 
             return url
