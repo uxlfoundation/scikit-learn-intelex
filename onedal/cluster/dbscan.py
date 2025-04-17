62a7966bb42c1c081e02786b4637b8501dd92389
Ian Faust <icfaust@gmail.com>
2025-04-18 01:02:42 +0200

    Update dbscan.py

--- onedal/cluster/dbscan.py
+++ onedal/cluster/dbscan.py
@@ -78,14 +78,14 @@ class DBSCAN(ClusterMixin):
         params = self._get_onedal_params(X_table.dtype)
         result = self.compute(params, X_table, sample_weight_table)
 
-        self.labels_ = from_table(result.responses, array=X).ravel()
+        self.labels_ = from_table(result.responses, like=X).ravel()
         if (
             result.core_observation_indices is not None
             and not result.core_observation_indices.kind == "empty"
         ):
             self.core_sample_indices_ = from_table(
                 result.core_observation_indices,
-                array=X,
+                like=X,
             ).ravel()
         else:
             # construct keyword arguments for different namespaces (dptcl takes sycl_queue)
