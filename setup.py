9db6ada3d3fea2bc546fb8b4fa7b1b021395b4c1
David Cortes <david.cortes@intel.com>
2025-04-18 10:37:25 +0200

    remove unneeded flag

--- setup.py
+++ setup.py
@@ -283,7 +283,7 @@ def get_build_options():
         '-DD4P_VERSION="' + sklearnex_version + '"',
         "-DNPY_ALLOW_THREADS=1",
     ]
-    ela = ["-no-intel-lib"] if using_intel else []
+    ela = []
 
     if using_intel and IS_WIN:
         include_dir_plat.append(
