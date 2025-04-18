e52a3ff5b836a90d0716ff9be8adc3c191782e96
David Cortes <david.cortes@intel.com>
2025-04-18 07:39:43 +0200

    correct argument

--- setup.py
+++ setup.py
@@ -285,7 +285,7 @@ def get_build_options():
         '-DD4P_VERSION="' + sklearnex_version + '"',
         "-DNPY_ALLOW_THREADS=1",
     ]
-    ela = ["-no-intel-libs"] if using_intel else []
+    ela = ["-no-intel-lib"] if using_intel else []
 
     if using_intel and IS_WIN:
         include_dir_plat.append(
