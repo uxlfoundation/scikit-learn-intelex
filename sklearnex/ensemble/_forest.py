46262ee148c5d93291caa454b8d89f457f3e20c6
ethanglaser <ethan.glaser@intel.com>
2025-06-04 01:54:16 +0200

    fix: forest dpctl predict queue misalignment

--- sklearnex/ensemble/_forest.py
+++ sklearnex/ensemble/_forest.py
@@ -826,7 +826,7 @@ class ForestClassifier(BaseForest, _sklearn_ForestClassifier):
         res = self._onedal_estimator.predict(X, queue=queue)
         try:
             return xp.take(
-                xp.asarray(self.classes_), xp.astype(xp.reshape(res, (-1,)), xp.int64)
+                xp.asarray(self.classes_, sycl_queue=res.sycl_queue), xp.astype(xp.reshape(res, (-1,)), xp.int64)
             )
         except AttributeError:
             return np.take(self.classes_, res.ravel().astype(np.int64, casting="unsafe"))
