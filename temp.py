3b871e704f72fe5b63717f39278668bab4940285
eglaser <ethan.glaser@intel.com>
2025-06-11 20:32:29 +0200

    oops

--- temp.py
+++
@@ -1,7 +0,0 @@
-if True:
-    with open('requirements-doc.txt', 'r') as infile, open('abc.txt', 'w') as outfile:
-        for line in infile:
-            line = line.strip()
-            if line and not line.startswith('#'):
-                package = line.split('==')[0].strip()
-                outfile.write(f"{package}\n")
