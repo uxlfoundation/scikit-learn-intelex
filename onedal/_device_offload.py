d4317b4ecf201e23c21d273c3c37c2a99074324d
Ian Faust <icfaust@gmail.com>
2025-06-18 13:34:14 +0200

    Update _device_offload.py

--- onedal/_device_offload.py
+++ onedal/_device_offload.py
@@ -24,7 +24,7 @@ from sklearn import get_config
 from onedal import _default_backend as backend
 
 from ._config import _get_config
-from .datatypes import copy_to_dpnp, copy_to_usm, kDLCPU, usm_to_numpy
+from .datatypes import copy_to_dpnp, copy_to_usm, usm_to_numpy
 from .utils import _sycl_queue_manager as QM
 from .utils._array_api import _asarray, _is_numpy_namespace
 from .utils._third_party import is_dpnp_ndarray
