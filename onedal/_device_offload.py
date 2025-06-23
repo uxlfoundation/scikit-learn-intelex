12cdbc81b05aa4f313b270d059f563ab00ae2d43
Ian Faust <icfaust@gmail.com>
2025-06-24 01:55:25 +0200

    Update _device_offload.py

--- onedal/_device_offload.py
+++ onedal/_device_offload.py
@@ -21,8 +21,6 @@ from functools import wraps
 import numpy as np
 from sklearn import get_config
 
-from onedal import _default_backend as backend
-
 from ._config import _get_config
 from .datatypes import copy_to_dpnp, copy_to_usm, dlpack_to_numpy, usm_to_numpy
 from .utils import _sycl_queue_manager as QM
@@ -30,7 +28,6 @@ from .utils._array_api import _asarray, _is_numpy_namespace
 from .utils._third_party import is_dpnp_ndarray
 
 logger = logging.getLogger("sklearnex")
-cpu_dlpack_device = (backend.kDLCPU, 0)
 
 
 def supports_queue(func):
