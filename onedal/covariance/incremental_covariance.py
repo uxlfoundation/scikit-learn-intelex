4e09f7db5154ad8c7c6eca104dd73195f50c9132
Ian Faust <icfaust@gmail.com>
2025-07-12 03:24:27 +0200

    Update incremental_covariance.py

--- onedal/covariance/incremental_covariance.py
+++ onedal/covariance/incremental_covariance.py
@@ -138,7 +138,7 @@ class IncrementalEmpiricalCovariance(BaseEmpiricalCovariance):
                 result = self.finalize_compute(params, self._partial_result)
 
             self.covariance_ = from_table(result.cov_matrix, like=self._outtype)
-            
+
             if self.bias and not daal_check_version((2024, "P", 1)):
                 n_rows = self._partial_result.partial_n_rows
                 self.covariance_ = self.covariance_ * (n_rows - 1) / n_rows
