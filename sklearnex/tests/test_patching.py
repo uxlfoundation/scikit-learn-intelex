34fec93ab0acdfc70e16f1055f26e5cf8317390e
Ian Faust <icfaust@gmail.com>
2025-07-12 02:41:40 +0200

    Update test_patching.py

--- sklearnex/tests/test_patching.py
+++ sklearnex/tests/test_patching.py
@@ -174,7 +174,11 @@ def test_standard_estimator_patching(caplog, dataframe, queue, dtype, estimator,
         # the infrastructure from sklearn that sklearnex depends on is also susceptible
         # to failure. In this case compare to sklearn for the same failure. By design
         # the patching of sklearn should act similarly. Technically this is conformance.
-        if not sklearn_check_version("1.5") and "Linear" in Estimator or "Ridge" in Estimator:
+        if (
+            not sklearn_check_version("1.5")
+            and "Linear" in Estimator
+            or "Ridge" in Estimator
+        ):
             pytest.skip(
                 f"y_numeric option to validate_data does not support array_api inputs for sklearn <1.5"
             )
@@ -182,11 +186,11 @@ def test_standard_estimator_patching(caplog, dataframe, queue, dtype, estimator,
             not sklearn_check_version("1.3")
             and estimator == "IncrementalEmpiricalCovariance"
             and method == "score"
-            ):
+        ):
             pytest.skip(
                 f"array checking in sklearn <1.3 does not fully support array_api inputs, causes sklearnex-only estimator failure"
             )
-        
+
         with config_context(array_api_dispatch=True):
             try:
                 _check_estimator_patching(caplog, dataframe, queue, dtype, est, method)
