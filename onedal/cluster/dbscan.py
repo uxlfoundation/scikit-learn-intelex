3dc8b85dec9df65082309454246b64952ccc84de
Faust, Ian <ian.faust@intel.com>
2025-07-14 23:59:38 +0200

    remove unused imports

--- onedal/cluster/dbscan.py
+++ onedal/cluster/dbscan.py
@@ -16,15 +16,11 @@
 
 import numpy as np
 
-from daal4py.sklearn._utils import make2d
 from onedal._device_offload import supports_queue
 
-from .._config import _get_config
 from ..common._backend import bind_default_backend
 from ..common._mixin import ClusterMixin
 from ..datatypes import from_table, to_table
-from ..utils._array_api import _get_sycl_namespace
-from ..utils.validation import _check_array
 
 
 class DBSCAN(ClusterMixin):
