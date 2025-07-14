fix covariance

sklearnex/preview/covariance/covariance.py
@@ -15,6 +15,7 @@
 # ===============================================================================
 
 import warnings
+from functools import partial
 
 import scipy.sparse as sp
 from sklearn.covariance import EmpiricalCovariance as _sklearn_EmpiricalCovariance
@@ -22,18 +23,22 @@ from sklearn.utils.validation import check_is_fitted
 
 from daal4py.sklearn._n_jobs_support import control_n_jobs
 from daal4py.sklearn._utils import daal_check_version, sklearn_check_version
+from daal4py.sklearn.metrics import pairwise_distances
 from onedal._device_offload import support_input_format
 from onedal.common.hyperparameters import get_hyperparameters
 from onedal.covariance import EmpiricalCovariance as onedal_EmpiricalCovariance
 from onedal.utils._array_api import _is_numpy_namespace
 from sklearnex import config_context
-from sklearnex.metrics import pairwise_distances
 
 from ..._device_offload import dispatch, wrap_output_data
 from ..._utils import PatchingConditionsChain, register_hyperparameters
 from ...base import oneDALEstimator
 from ...utils._array_api import enable_array_api, get_namespace, log_likelihood, pinvh
 from ...utils.validation import validate_data
+
+# This is a temporary workaround for issues with sklearnex._device_offload._get_host_inputs
+# passing kwargs with sycl queues with other host data will cause failures
+_mahalanobis = support_input_format(partial(pairwise_distances, metric="mahalanobis"))
 
 
 @enable_array_api