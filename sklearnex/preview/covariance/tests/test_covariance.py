9c95c59f6906eb0ebfb49106e2d0bcf32de39642
david-cortes-intel <david.cortes@intel.com>
2025-07-21 14:00:58 +0200

    ENH: Add parameters for covariance and PCA controlling batched vs. non-batched route (#2568)
    * add parameters about batched/non-batched route for covariance and PCA
    
    * switch to new resetter

--- sklearnex/preview/covariance/tests/test_covariance.py
+++ sklearnex/preview/covariance/tests/test_covariance.py
@@ -85,3 +85,28 @@ def test_sklearnex_import_covariance(
 
     assert_allclose(expected_covariance, result.covariance_)
     assert_allclose(expected_means, result.location_)
+
+
+@pytest.mark.skipif(
+    not daal_check_version((2025, "P", 700)),
+    reason="Functionality introduced in a later version",
+)
+@pytest.mark.parametrize("dataframe,queue", get_dataframes_and_queues())
+@pytest.mark.parametrize("assume_centered", [True, False])
+def test_non_batched_covariance(hyperparameters, dataframe, queue, assume_centered):
+    if queue and queue.sycl_device.is_gpu:
+        pytest.skip("Test for CPU-only functionality")
+
+    from sklearnex.preview.covariance import EmpiricalCovariance
+
+    X = np.random.default_rng(seed=123).random(size=(20, 5))
+
+    hyperparameters.cpu_max_cols_batched = np.iinfo(np.int32).max
+    res_batched = EmpiricalCovariance(assume_centered=assume_centered).fit(X).covariance_
+
+    hyperparameters.cpu_max_cols_batched = 1
+    res_non_batched = (
+        EmpiricalCovariance(assume_centered=assume_centered).fit(X).covariance_
+    )
+
+    np.testing.assert_allclose(res_non_batched, res_batched)
