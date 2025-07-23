40f2ca4af520967bfd6476fb5d98a77b8b0892b4
Ethan Glaser <ethan.glaser@intel.com>
2025-07-22 17:11:38 -0700

    attempt to resolve sklearn get_param_names runtime error

--- sklearnex/spmd/ensemble/forest.py
+++ sklearnex/spmd/ensemble/forest.py
@@ -25,9 +25,58 @@ class RandomForestClassifier(RandomForestClassifier_Batch):
     __doc__ = RandomForestClassifier_Batch.__doc__
     _onedal_factory = onedal_RandomForestClassifier
 
-    def __init__(self, *args, local_trees_mode=False, **kwargs):
+    #def __init__(self, *args, local_trees_mode=False, **kwargs):
+    # TODO init based on sklearn version
+    def __init__(
+        self,
+        n_estimators=100,
+        *,
+        criterion="gini",
+        max_depth=None,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.0,
+        max_features="sqrt",
+        max_leaf_nodes=None,
+        min_impurity_decrease=0.0,
+        bootstrap=True,
+        oob_score=False,
+        n_jobs=None,
+        random_state=None,
+        verbose=0,
+        warm_start=False,
+        class_weight=None,
+        ccp_alpha=0.0,
+        max_samples=None,
+        monotonic_cst=None,
+        max_bins=256,
+        min_bin_size=1,
+        local_trees_mode=False,
+    ):
         self.local_trees_mode = local_trees_mode
-        super().__init__(*args, **kwargs)
+        super().__init__(
+            n_estimators=n_estimators,
+            criterion=criterion,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            min_weight_fraction_leaf=min_weight_fraction_leaf,
+            max_features=max_features,
+            max_leaf_nodes=max_leaf_nodes,
+            min_impurity_decrease=min_impurity_decrease,
+            bootstrap=bootstrap,
+            oob_score=oob_score,
+            n_jobs=n_jobs,
+            random_state=random_state,
+            verbose=verbose,
+            warm_start=warm_start,
+            class_weight=class_weight,
+            ccp_alpha=ccp_alpha,
+            max_samples=max_samples,
+            monotonic_cst=monotonic_cst,
+            max_bins=max_bins,
+            min_bin_size=min_bin_size,
+        )
 
     def _create_onedal_estimator(self, onedal_params):
         onedal_params = dict(onedal_params)  # copy to avoid mutating input
@@ -59,9 +108,56 @@ class RandomForestRegressor(RandomForestRegressor_Batch):
     __doc__ = RandomForestRegressor_Batch.__doc__
     _onedal_factory = onedal_RandomForestRegressor
 
-    def __init__(self, *args, local_trees_mode=False, **kwargs):
+    # def __init__(self, *args, local_trees_mode=False, **kwargs):
+
+    def __init__(
+        self,
+        n_estimators=100,
+        *,
+        criterion="squared_error",
+        max_depth=None,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.0,
+        max_features=1.0,
+        max_leaf_nodes=None,
+        min_impurity_decrease=0.0,
+        bootstrap=True,
+        oob_score=False,
+        n_jobs=None,
+        random_state=None,
+        verbose=0,
+        warm_start=False,
+        ccp_alpha=0.0,
+        max_samples=None,
+        monotonic_cst=None,
+        max_bins=256,
+        min_bin_size=1,
+        local_trees_mode=False,
+    ):
         self.local_trees_mode = local_trees_mode
-        super().__init__(*args, **kwargs)
+        super().__init__(
+            n_estimators=n_estimators,
+            criterion=criterion,
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            min_weight_fraction_leaf=min_weight_fraction_leaf,
+            max_features=max_features,
+            max_leaf_nodes=max_leaf_nodes,
+            min_impurity_decrease=min_impurity_decrease,
+            bootstrap=bootstrap,
+            oob_score=oob_score,
+            n_jobs=n_jobs,
+            random_state=random_state,
+            verbose=verbose,
+            warm_start=warm_start,
+            ccp_alpha=ccp_alpha,
+            max_samples=max_samples,
+            monotonic_cst=monotonic_cst,
+            max_bins=max_bins,
+            min_bin_size=min_bin_size,
+        )
 
     def _create_onedal_estimator(self, onedal_params):
         onedal_params = dict(onedal_params)  # copy to avoid mutating input
