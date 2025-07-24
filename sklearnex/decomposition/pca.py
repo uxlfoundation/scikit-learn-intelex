c61281c1c3e1b968f227ea23c324b94b09ef3909
icfaust <icfaust@gmail.com>
2025-07-24 04:07:39 -0700

    remove reshape, must disable tests

--- sklearnex/decomposition/pca.py
+++ sklearnex/decomposition/pca.py
@@ -368,33 +368,23 @@ if daal_check_version((2024, "P", 100)):
             self.n_components_ = n_components
             self.noise_variance_ = self._compute_noise_variance(xp)
 
-            self.components_ = xp.reshape(
-                self._onedal_estimator.components_[:n_components, ...],
-                (n_components, -1),
-                copy=True,
-            )
-            self.explained_variance_ = xp.reshape(
-                self._onedal_estimator.explained_variance_[:n_components],
-                (-1,),
-                copy=True,
-            )
-            self.mean_ = xp.reshape(self._onedal_estimator.mean_, shape=(-1,), copy=True)
+            self.components_ = self._onedal_estimator.components_[:n_components, ...]
+            self.explained_variance_ = self._onedal_estimator.explained_variance_[
+                :n_components
+            ]
+            self.mean_ = self._onedal_estimator.mean_
 
             # set other fit attributes, first by modifying the onedal_estimator
-            self._onedal_estimator.singular_values_ = xp.reshape(
-                self._onedal_estimator.singular_values_[:n_components],
-                (-1,),
-                copy=True,
+            self._onedal_estimator.singular_values_ = (
+                self._onedal_estimator.singular_values_[:n_components]
             )
-            self._onedal_estimator.explained_variance_ratio_ = xp.reshape(
-                self._onedal_estimator.explained_variance_ratio_[:n_components],
-                (-1,),
-                copy=True,
+            self._onedal_estimator.explained_variance_ratio_ = (
+                self._onedal_estimator.explained_variance_ratio_[:n_components]
             )
 
             self.singular_values_ = self._onedal_estimator.singular_values_
-            self.explained_variance_ratio_ = xp.reshape(
-                self._onedal_estimator.explained_variance_ratio_, shape=(-1,), copy=True
+            self.explained_variance_ratio_ = (
+                self._onedal_estimator.explained_variance_ratio_
             )
 
             # return X for use in fit_transform, as it is validated and ready
