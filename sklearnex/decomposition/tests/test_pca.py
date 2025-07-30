341e6855a998ae5873b606430293574abb568489
icfaust <icfaust@gmail.com>
2025-07-29 21:22:02 -0700

    add requested testing

--- sklearnex/decomposition/tests/test_pca.py
+++ sklearnex/decomposition/tests/test_pca.py
@@ -16,7 +16,10 @@
 
 import numpy as np
 import pytest
-from numpy.testing import assert_allclose
+import scipy.sparse as sp
+from numpy.testing import array_equal, assert_allclose
+from sklearn.base import clone
+from sklearn.datasets import load_iris
 
 from daal4py.sklearn._utils import daal_check_version
 from onedal.tests.utils._dataframes_support import (
@@ -107,4 +110,53 @@ def test_non_batched_covariance(hyperparameters, dataframe, queue):
     hyperparameters.cpu_max_cols_batched = 1
     res_non_batched = PCA().fit(X).components_
 
-    np.testing.assert_allclose(res_non_batched, res_batched)
+    assert_allclose(res_non_batched, res_batched)
+
+
+@pytest.mark.parametrize("dataframe,queue", get_dataframes_and_queues())
+def test_changed_estimated_attributes(with_array_api, dataframe, queue):
+    if dataframe == "pandas":
+        pytest.skip("pandas data not supported with array_api_dispatch")
+    # check that attributes necessary for the PCA onedal estimator match
+    # changes occuring in the sklearnex estimator
+    X, y = load_iris(return_X_y=True)
+
+    X_0 = _convert_to_dataframe(X[y == 0], sycl_queue=queue, target_df=dataframe)
+    X = _convert_to_dataframe(X, sycl_queue=queue, target_df=dataframe)
+
+    est0 = PCA(n_components=4).fit(X_0)
+    est = clone(est0)
+    assert not hasattr(est, "_onedal_estimator")
+    est.fit(X)
+    assert not array_equal(_as_numpy(est.transform(X)), _as_numpy(est0.transform(X)))
+
+    # copy over parameters necessary for transform
+    est.mean_ = est0.mean_
+    est.components_ = est0.components_
+    est.explained_variance_ = est0.explained_variance_
+    est.n_components_ = est0.n_components_  # is trivial but excercises the logic
+
+    assert array_equal(_as_numpy(est.transform(X)), _as_numpy(est0.transform(X)))
+
+
+@pytest.mark.allow_sklearn_fallback
+def test_create_model_behavior():
+    # verify that fit fallbacks does not break ``transform`` as the oneDAL
+    # model is generated JIT
+
+    X, _ = load_iris(return_X_y=True)
+    # generate a onedal estimator
+    est = PCA()
+    X_trans = est.fit_transform(X)
+
+    # force data to sparse for a fallback to sklearn
+    X_sp = sp.csr_array(X)
+    est.fit(X_sp)
+    # In the case of a fallback, the model should be set to none by clobbered
+    # fitted attributes
+    assert est._onedal_estimator._onedal_model is None
+
+    X_trans_sparse = est.transform(X)
+    # use allclose as data was fit with sklearn and onedal on the same data
+    # but using different backends
+    assert_allclose(X_trans, X_trans_sparse)
