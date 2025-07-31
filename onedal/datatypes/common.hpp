36b4ea548f9393adc6eeec162c545868ee44a2c9
icfaust <icfaust@gmail.com>
2025-07-31 01:45:37 -0700

    oops

---
+++ onedal/datatypes/common.hpp
@@ -0,0 +1,43 @@
+/*******************************************************************************
+* Copyright Contributors to the oneDAL project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include "oneapi/dal/array.hpp"
+
+template <typename T>
+static dal::array<T> transfer_to_host(const dal::array<T> &array) {
+#ifdef ONEDAL_DATA_PARALLEL
+    auto opt_queue = array.get_queue();
+    if (opt_queue.has_value()) {
+        auto device = opt_queue->get_device();
+        if (!device.is_cpu()) {
+            const auto *device_data = array.get_data();
+
+            auto memory_kind = sycl::get_pointer_type(device_data, opt_queue->get_context());
+            if (memory_kind == sycl::usm::alloc::unknown) {
+                throw std::runtime_error("[convert_to_numpy] Unknown memory type");
+            }
+            if (memory_kind == sycl::usm::alloc::device) {
+                auto host_array = dal::array<T>::empty(array.get_count());
+                opt_queue->memcpy(host_array.get_mutable_data(), device_data, array.get_size())
+                    .wait_and_throw();
+                return host_array;
+            }
+        }
+    }
+#endif
+
+    return array;
+}
