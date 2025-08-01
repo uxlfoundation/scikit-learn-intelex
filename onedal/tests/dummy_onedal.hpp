c6e8487471dc12e96290b12d7c330aa8bdafde37
Faust, Ian <ian.faust@intel.com>
2025-08-01 16:38:33 +0200

    small fix

--- onedal/tests/dummy_onedal.hpp
+++ onedal/tests/dummy_onedal.hpp
@@ -199,7 +199,8 @@ struct train_ops {
                     const input_t& input) const {
         // Usually a infer_ops_dispatcher is contained in oneDAL infer_ops.cpp.
         // Due to the simplicity of this algorithm, implement it here.
-        auto queue = ctx.get_queue() dal::array<float_t> array =
+        auto queue = ctx.get_queue();
+         dal::array<float_t> array =
             dal::array::full(queue, 1, desc.get_constant());
         result_t result();
         result.data = dal::homogen_table::wrap(array, 1, 1);
