c22648069b7040069a85a41eb7a5e510465027fa
Faust, Ian <ian.faust@intel.com>
2025-09-22 13:13:48 +0200

    fix again

--- tests/test_model_builders.py
+++ tests/test_model_builders.py
@@ -2180,7 +2180,7 @@ def test_logreg_builder_sequential_calls():
             ),
         ]
         if not sklearn_check_version("1.8")
-        else [[(LogisticRegression(solver="liblinear"), 2)]]
+        else [(LogisticRegression(solver="liblinear"), 2)]
     )
     + [
         # case below might change in the future if sklearn improves their modules
