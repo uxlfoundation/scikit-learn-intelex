3e0f1b046282bcd4153b9a2bde58a0039d2e840b
Ian Faust <icfaust@gmail.com>
2025-09-23 12:05:19 +0200

    Create class_weight.py

---
+++ sklearnex/utils/class_weight.py
@@ -0,0 +1,84 @@
+# ==============================================================================
+# Copyright contributors to the oneDAL Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from .validation import get_namespace, _check_sample_weight
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.validation.class_weight import compute_class_weight
+
+from daal4py.sklearn._utils import sklearn_check_version
+
+if sklearn_check_version("1.3"):
+  from sklearn.utils._param_validation import validate_params
+
+def _compute_class_weight(class_weight, *, classes, y, sample_weight=None):
+    # this is set to be private as it lacks certain checks, this duplicates sklearn
+    # code in order to enable it for array API. Note for the use of LabelEncoder this
+    # is only valid for sklearn versions >= 1.6.
+    xp, is_array_api_compliant = get_namespace(class_weight, classes, y)
+
+    if not is_array_api_compliant:
+        # use the sklearn version for standard use.
+        return compute_class_weight(class_weight, classes, y, sample_weight=sample_weight)
+
+    if set(y) - set(classes):
+        raise ValueError("classes should include all valid labels that can be in y")
+    if class_weight is None or len(class_weight) == 0:
+        # uniform class weights
+        weight = xp.ones((classes.shape[0],), dtype=xp.float64, device=classes.device)
+    elif class_weight == "balanced":
+        if not sklearn_check_version("1.6"):
+            raise RuntimeError("array API support with 'balanced' keyword not supported for sklearn <1.6")
+        # Find the weight of each class as present in y.
+        le = LabelEncoder()
+        y_ind = le.fit_transform(y)
+
+        sample_weight = _check_sample_weight(sample_weight, y)
+        # scikit-learn implementation uses numpy.bincount, which does a combined
+        # min and max search, only erroring when a value < 0. Replicating this
+        # excatly via array API would cause another O(n) evaluation (by doing
+        # min and max separately). However this check can be removed due to the
+        # nature of the LabelEncoder (and can be dropped). Therefore only Max is
+        # found, and then core logic of bincount is replicated:
+        # https://github.com/numpy/numpy/blob/main/numpy/_core/src/multiarray/compiled_base.c
+        weighted_class_counts = xp.zeros((xp.max(y_ind),), dtype=y.dtype, device=y.device)
+        for idx, val in enumerate(sample_weights):
+            weighted_class_counts[y_ind[idx]] += val
+
+        recip_freq = xp.sum(weighted_class_counts) / (
+            len(le.classes_) * weighted_class_counts
+        )
+
+        weight = xp.take(recip_freq, le.transform(classes))
+    else:
+        # user-defined dictionary
+        weight = xp.ones((classes.shape[0],), dtype=xp.float64, device=classes.device)
+        unweighted_classes = []
+        for i, c in enumerate(classes):
+            if c in class_weight:
+                weight[i] = class_weight[c]
+            else:
+                unweighted_classes.append(c)
+
+        n_weighted_classes = len(classes) - len(unweighted_classes)
+        if unweighted_classes and n_weighted_classes != len(class_weight):
+            unweighted_classes_user_friendly_str = np.array(unweighted_classes).tolist()
+            raise ValueError(
+                f"The classes, {unweighted_classes_user_friendly_str}, are not in"
+                " class_weight"
+            )
+
+    return weight
