5d92227eb69167c2a6a67109446a25bf9c4e1875
Faust, Ian <ian.faust@intel.com>
2025-10-01 09:43:17 +0200

    make check for sklearn 1.7 in testing

--- sklearnex/utils/tests/test_class_weight.py
+++ sklearnex/utils/tests/test_class_weight.py
@@ -43,10 +43,13 @@ def test_compute_class_weight_array_api(class_weight, dataframe, queue):
 
     rng = np.random.default_rng(seed=42)
 
-    sample_weight = (
-        rng.random(y.shape).astype(np.float64) if class_weight == "balanced" else None
-    )
+    # support of sample_weights added in sklearn 1.7
+    set_sample_weight = class_weight == "balanced" and sklearn_check_version("1.7")
 
+    sample_weight = (
+        rng.random(y.shape).astype(np.float64) if set_sample_weight else None
+    )
+    
     if class_weight == "ramp":
         class_weight = {int(i): int(i) for i in np.unique(y)}
 
@@ -54,7 +57,7 @@ def test_compute_class_weight_array_api(class_weight, dataframe, queue):
         class_weight, classes=classes, y=y, sample_weight=sample_weight
     )
 
-    if class_weight == "balanced":
+    if set_sample_weight:
         sample_weight = _convert_to_dataframe(
             sample_weight, target_df=dataframe, device=queue
         )
