f3d2ec04626420114654d386522876c4baa0c5cd
Faust, Ian <ian.faust@intel.com>
2025-10-20 22:56:37 +0200

    remove array

--- onedal/ensemble/forest.py
+++ onedal/ensemble/forest.py
@@ -231,7 +231,8 @@ class ForestClassifier(BaseForest):
             X, y, sample_weight=sample_weight, class_count=class_count, queue=queue
         )
 
-        self.oob_score_ = float(from_table(train_result.oob_err_accuracy, like=X)[0, 0])
+        self.oob_score_ = from_table(train_result.oob_err_accuracy, like=X)
+        self.oob_score_ = self.oob_score_[(0,)*self.oob_score_.ndim]
         self.oob_decision_function_ = from_table(
             train_result.oob_err_decision_function, like=X
         )
@@ -247,8 +248,9 @@ class ForestRegressor(BaseForest):
             X, y, sample_weight=sample_weight, class_count=class_count, queue=queue
         )
 
-        self.oob_score_ = float(from_table(train_result.oob_err_r2)[0, 0])
-        self.oob_prediction_ = from_table(train_result.oob_err_prediction)
+        self.oob_score_ = from_table(train_result.oob_err_r2, like=X)
+        self.oob_score_ = self.oob_score_[(0,)*self.oob_score_.ndim]
+        self.oob_prediction_ = from_table(train_result.oob_err_prediction, like=X)
 
         return self
 
