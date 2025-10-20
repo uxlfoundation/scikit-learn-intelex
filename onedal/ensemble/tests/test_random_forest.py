d25ba9ffc55eaf9491e0984420cd3b30ba899804
Faust, Ian <ian.faust@intel.com>
2025-10-20 23:54:56 +0200

    updates

--- onedal/ensemble/tests/test_random_forest.py
+++ onedal/ensemble/tests/test_random_forest.py
@@ -15,7 +15,6 @@
 # ==============================================================================
 
 import numpy as np
-
 import pytest
 from numpy.testing import assert_allclose
 from sklearn.datasets import make_classification, make_regression
