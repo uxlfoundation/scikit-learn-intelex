9e5a5ad7330440f84124de2c73ca03696e964d33
Faust, Ian <ian.faust@intel.com>
2025-10-21 14:45:36 +0200

    updates

--- sklearnex/tests/utils/base.py
+++ sklearnex/tests/utils/base.py
@@ -398,8 +398,8 @@ def _get_processor_info():
 
 
 class DummyEstimator(BaseEstimator):
-        
-    def fit(self, X, y):
+
+    def fit(self, X, y=None):
         X_table = to_table(X)
         y_table = to_table(y)
         # The presence of the fitted attributes (ending with a trailing
