f349ad2e4c1eebebaa2c05a1606aebfb66a5f5a3
Alexandr-Solovev <aleksandr.solovev@intel.com>
2026-03-16 01:45:02 -0700

    fixes for pandas

--- doc/sources/conf.py
+++ doc/sources/conf.py
@@ -86,7 +86,7 @@ intersphinx_mapping = {
     "numpy": ("https://numpy.org/doc/stable", None),
     "scipy": ("https://docs.scipy.org/doc/scipy/", None),
     "matplotlib": ("https://matplotlib.org/stable/", None),
-    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
     "joblib": ("https://joblib.readthedocs.io/en/latest/", None),
     "seaborn": ("https://seaborn.pydata.org/", None),
     "skops": ("https://skops.readthedocs.io/en/stable/", None),
