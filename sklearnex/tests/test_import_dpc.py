9904c5140c9a3e209f44abb19beaff5d5df86a70
David Cortes <david.cortes@intel.com>
2026-03-18 16:16:24 +0100

    try another way

--- sklearnex/tests/test_import_dpc.py
+++ sklearnex/tests/test_import_dpc.py
@@ -4,8 +4,7 @@ import pathlib
 
 def test_import_onedal_py_dpc():
     import onedal
-    import onedal._onedal_py_host
 
-    print(os.listdir(pathlib.Path(onedal._onedal_py_host.__file__).parent.resolve()))
+    print(os.listdir(pathlib.Path(onedal.__file__).parent.resolve()))
     print("MKLROOT:", os.environ.get("MKLROOT", None))
     import onedal._onedal_py_dpc
