7278d2f953e8bf82c3b6ef35f1d5405f6d30cf85
David Cortes <david.cortes@intel.com>
2026-03-20 17:33:23 +0100

    don't try to print windows-only info

--- sklearnex/tests/test_import_dpc.py
+++ sklearnex/tests/test_import_dpc.py
@@ -8,10 +8,5 @@ def test_import_onedal_py_dpc():
     print(os.listdir(pathlib.Path(onedal.__file__).parent.resolve()))
     print("MKLROOT:", os.environ.get("MKLROOT", None))
     print("PATH:", os.environ.get("PATH", None))
-    print(
-        "MKL contents:",
-        os.listdir(
-            "D://a//scikit-learn-intelex//scikit-learn-intelex//oneapi//mkl//latest//bin"
-        ),
-    )
+    print("DPCFLAG:", os.environ.get("DPCFLAG", None))
     import onedal._onedal_py_dpc
