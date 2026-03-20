b884eede0d5db0be3a1c2b75999c3497252e3789
David Cortes <david.cortes@intel.com>
2026-03-20 08:47:16 +0100

    print more info

--- sklearnex/tests/test_import_dpc.py
+++ sklearnex/tests/test_import_dpc.py
@@ -8,4 +8,10 @@ def test_import_onedal_py_dpc():
     print(os.listdir(pathlib.Path(onedal.__file__).parent.resolve()))
     print("MKLROOT:", os.environ.get("MKLROOT", None))
     print("PATH:", os.environ.get("PATH", None))
+    print(
+        "MKL contents:",
+        os.listdir(
+            "D://a//scikit-learn-intelex//scikit-learn-intelex//oneapi//mkl//latest//bin"
+        ),
+    )
     import onedal._onedal_py_dpc
