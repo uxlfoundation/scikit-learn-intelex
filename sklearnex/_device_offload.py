de8a64eee548ff6bbbee8257b5fb00d82968776e
yuejiaointel <yue.jiao@intel.com>
2026-03-27 16:36:27 -0700

    fix: skip output conversion when transform_output is non-default

--- sklearnex/_device_offload.py
+++ sklearnex/_device_offload.py
@@ -183,7 +183,8 @@ def wrap_output_data(func: Callable) -> Callable:
         result = func(self, *args, **kwargs)
         if not (len(args) == 0 and len(kwargs) == 0):
             data = (*args, *kwargs.values())[0]
-            # Remove check for result __sycl_usm_array_interface__ on deprecation of use_raw_inputs
+            if get_config().get("transform_output") not in ("default", None):
+                return result
             if (
                 usm_iface := getattr(data, "__sycl_usm_array_interface__", None)
             ) and not hasattr(result, "__sycl_usm_array_interface__"):
