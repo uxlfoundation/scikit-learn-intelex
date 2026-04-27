47ba23f3a2acbee572cb0bb9fc46be03bb14785c
david-cortes-intel <david.cortes@intel.com>
2026-04-27 00:46:43 -0700

    always expect error

--- sklearnex/tests/test_config.py
+++ sklearnex/tests/test_config.py
@@ -140,11 +140,7 @@ def test_host_backend_target_offload(target):
     err_msg = r"DPC"
 
     est = NearestNeighbors()
-    if target != "auto":
-        with pytest.raises(ValueError, match=err_msg):
-            with sklearnex.config_context(target_offload=target):
-                est.fit(np.eye(5, 8))
-    else:
+    with pytest.raises(ValueError, match=err_msg):
         with sklearnex.config_context(target_offload=target):
             est.fit(np.eye(5, 8))
 
