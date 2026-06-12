acc2409efab8b2c8cecc9879aaf21d0255a5ec61
Ian Faust <icfaust@gmail.com>
2026-06-12 13:56:37 +0200

    Update validation.py

--- onedal/utils/validation.py
+++ onedal/utils/validation.py
@@ -55,39 +55,6 @@ def _is_arraylike_not_scalar(array):
     return _is_arraylike(array) and not np.isscalar(array)
 
 
-def _is_integral_float(y):
-    return y.dtype.kind == "f" and np.all(y.astype(int) == y)
-
-
-def _is_multilabel(y):
-    if hasattr(y, "__array__") or isinstance(y, Sequence):
-        # DeprecationWarning will be replaced by ValueError, see NEP 34
-        # https://numpy.org/neps/nep-0034-infer-dtype-is-object.html
-        with warnings.catch_warnings():
-            warnings.simplefilter("error", VisibleDeprecationWarning)
-            try:
-                y = np.asarray(y)
-            except VisibleDeprecationWarning:
-                # dtype=object should be provided explicitly for ragged arrays,
-                # see NEP 34
-                y = np.array(y, dtype=object)
-
-    if not (hasattr(y, "shape") and y.ndim == 2 and y.shape[1] > 1):
-        return False
-
-    if sp.issparse(y):
-        if isinstance(y, (sp.dok_matrix, sp.lil_matrix)):
-            y = y.tocsr()
-        return (
-            len(y.data) == 0
-            or np.unique(y.data).size == 1
-            and (y.dtype.kind in "biu" or _is_integral_float(np.unique(y.data)))
-        )
-    labels = np.unique(y)
-
-    return len(labels) < 3 and (y.dtype.kind in "biu" or _is_integral_float(labels))
-
-
 def _check_n_features(self, X, reset):
     try:
         n_features = _num_features(X)
