125a06d82b664fcef3c0b28b6b4e3c7cc8bb0046
david-cortes-intel <david.cortes@intel.com>
2026-08-25 02:16:33 -0700

    restore formatting

--- sklearnex/linear_model/tests/test_mixed_inputs.py
+++ sklearnex/linear_model/tests/test_mixed_inputs.py
@@ -190,13 +190,7 @@ def test_error_on_incompatible_namespaces(
 @pytest.mark.parametrize("estimator_class", all_estimators)
 @pytest.mark.parametrize("use_partial_fit", [False, True])
 def test_linreg_mixed_devices(
-    X_xp,
-    y_xp,
-    X_device,
-    y_device,
-    estimator_class,
-    use_partial_fit,
-    with_array_api,
+    X_xp, y_xp, X_device, y_device, estimator_class, use_partial_fit, with_array_api
 ):
     if use_partial_fit and estimator_class in non_incremental_estimators:
         pytest.skip("partial_fit only for incremental estimators")
