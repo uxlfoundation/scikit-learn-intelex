ea705cc0f46f39c0da6ec972af3d389f499db5fa
ethanglaser <ethan.glaser@intel.com>
2026-09-17 14:56:54 -0700

    fix numpydoc formatting

--- sklearnex/dispatcher.py
+++ sklearnex/dispatcher.py
@@ -461,16 +461,6 @@ def patch_sklearn(
     versions of estimators and functions from the |sklearnex|, either as a whole
     or on a per-estimator basis.
 
-    Notes
-    -----
-    If estimators from ``sklearn`` have already been imported before ``patch_sklearn``
-    is called, they need to be re-imported in order for the patching to take effect.
-
-    See Also
-    --------
-    is_patched_instance: To verify that an instance of an estimator is patched.
-    unpatch_sklearn: To undo the patching.
-
     Parameters
     ----------
     name : str, list of str, or None
@@ -505,6 +495,16 @@ def patch_sklearn(
 
         If environment variable ``SKLEARNEX_PREVIEW`` is set at the moment this function
         is called, preview estimators will be patched regardless.
+
+    See Also
+    --------
+    is_patched_instance: To verify that an instance of an estimator is patched.
+    unpatch_sklearn: To undo the patching.
+
+    Notes
+    -----
+    If estimators from ``sklearn`` have already been imported before ``patch_sklearn``
+    is called, they need to be re-imported in order for the patching to take effect.
 
     Examples
     --------
